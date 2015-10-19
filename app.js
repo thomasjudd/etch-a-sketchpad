10e908be1a0b5ec2d4d1413210aff9c0baa3e79d
Thomas Judd <judd.p.th@gmail.com>
2015-10-19 10:42:52 -0400

    divs generated and found by css styles

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-document.write("Hello, World from javascript");
+//document.write("Hello, World from javascript");
+
+var outerDivOpen = "<div class='outer'>outerdiv";
+//var div = document.createElement("div");
+var divClose = "</div>";
+var innerDiv = "<div class='inner'>innerdiv</div>";
+var containerOpen = "<div id='container'>";
+
+document.write(containerOpen);
+for(i = 0; i < 16; i++){
+    document.write(outerDivOpen);
+    for(j = 0; j < 16; j++){
+        document.write(innerDiv);
+    }
+    document.write(divClose);
+}
+document.write(divClose);
